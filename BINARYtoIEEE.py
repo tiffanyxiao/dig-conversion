5aad755568106cc10700e391e3533c81e2260b6e
Tiffany Xiao <txiao8415@bths.edu>
2018-03-24 22:42:07 -0400

    Added from binary to IEEE converter.

---
+++ BINARYtoIEEE.py
@@ -0,0 +1,50 @@
+# need to change 32 later on
+def hexConvert(value, width):
+    width = int(width)
+    b = bin(int(value, 16))
+    s = b[2:].zfill(width)
+    return ('{:0'+str(width)+'b}').format(int(value, 16))
+
+def exponent(e, bias):
+    # convert e to decimal number
+    convertExponent = int(e, 2)
+    # subtract bias
+    print("e", convertExponent)
+    return convertExponent - int(bias)
+
+def mantissa(m, mantissaLength):
+    result = 0
+    m = '0.'+m
+    for i in range(1,int(mantissaLength)):
+        result += int(m[2+i])*(2**(-i))
+    print(result)
+    return result
+
+def main():
+    num = input("Input number: ")
+
+    # convert to hex if needed
+    hex = input("y for Hex, n for binary: ")
+    if (hex == 'y'):
+        width = input("Input width of number: ")
+        num = hexConvert(num, width)
+
+    # get the sign
+    s = int(num[0])
+
+    # get the exponent
+    exponentLength = input("Input width of exponent (8 by default): ")
+    e = num[1:int(exponentLength)+1]
+    # get the decimal value of the exponent
+    bias = input("Input bias/excess (default 127): ")
+    decE = exponent(e, bias)
+
+    # get the mantissa
+    mantissaLength = input("Input width of mantissa (23 by default): ")
+    m = num[int(exponentLength):int(exponentLength)+int(mantissaLength)]
+    decM = mantissa(m, mantissaLength)
+
+    finalNum = ((-1)**(s))*(1+decM)*(2**decE)
+    print(finalNum)
+
+main()
